4e4daab30805c7acf12921c17722bfd5dfe6f3cd
Florian Morel <florian@hellohikimori.com>
2014-07-28 11:08:06 +0200

    Added add/removeClass

---
+++ README.md
@@ -0,0 +1,7 @@
+vue-query
+=========
+
+Vue.js plugin - DOM utils
+Add this.$findOne, this.$find, this.add/removeClass to any Vue instance.
+
+Inspired by https://gist.github.com/bpierre/6c98f2249b4e6f55edd1
---
+++ index.js
@@ -0,0 +1,40 @@
+'use strict';
+
+var slice = [].slice;
+
+function find(el, selector) {
+  return el.querySelector(selector);
+}
+
+function findAll(el, selector) {
+  el = el || document;
+  return slice.call(el.querySelectorAll(selector));
+}
+
+function hasClass(el, className) {
+    return new RegExp(' ' + className + ' ').test(' ' + el.className + ' ');
+}
+
+exports.install = function(Vue) {
+  var utils = Vue.require('utils');
+  utils.extend(Vue.prototype, {
+    $findOne: function(selector) {
+        return find(this.$el, selector);
+    },
+    $find: function(selector) {
+        return findAll(this.$el, selector);
+    },
+    hasClass: function(className, selector) {
+        var el = selector ? this.$el.$findOne(selector) : this.$el;
+        return hasClass(el, className);
+    },
+    addClass: function(className, selector) {
+        var el = selector ? this.$el.$findOne(selector) : this.$el;
+        return utils.addClass(el, className);
+    },
+    removeClass: function(className, selector) {
+        var el = selector ? this.$el.$findOne(selector) : this.$el;
+        return utils.removeClass(el, className);
+    }
+  });
+};
