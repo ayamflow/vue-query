f5377f2592e733d3bd109b73f0d6540ae024a595
Guillaume Gouessan <guillaume.gouessan@gmail.com>
2014-08-06 16:08:32 +0200

    add some doc and addClass and removeClass can now act on a set of elements, not just one

--- index.js
+++ index.js
@@ -15,26 +15,72 @@ function hasClass(el, className) {
     return new RegExp(' ' + className + ' ').test(' ' + el.className + ' ');
 }
 
+function isArray(object) {
+    return Object.prototype.toString.call(object) === '[object Array]';
+}
+
 exports.install = function(Vue) {
   var utils = Vue.require('utils');
   utils.extend(Vue.prototype, {
+    /**
+     * Return a single dom element from the current VM matching the given selector
+     * @param  {string} selector string selector to search
+     * @return {domElement}          the VM's child found
+     */
     $findOne: function(selector) {
         return find(this.$el, selector);
     },
+
+    /**
+     * Return an array of domElement from the current VM matching the given selector
+     * @param  {string} selector string selector to search
+     * @return {array}          array containing domElements found in the VM
+     */
     $find: function(selector) {
         return findAll(this.$el, selector);
     },
+
+    /**
+     * Check if the current VM has a given class, if a selector is passed as second parameters, we'll check the corresponding child instead
+     * @param  {string}  className
+     * @param  {string}  selector
+     * @return {Boolean}
+     */
     hasClass: function(className, selector) {
-        var el = selector ? this.$el.$findOne(selector) : this.$el;
+        var el = selector ? this.$findOne(selector) : this.$el;
         return hasClass(el, className);
     },
+
+    /**
+     * Add a class to the current VM or to its child matching 'selector'
+     * @param {string} className
+     * @param {string} selector
+     */
     addClass: function(className, selector) {
         if(this.hasClass(className, selector)) return;
-        var el = selector ? this.$el.$findOne(selector) : this.$el;
+        var el = selector ? this.$find(selector) : this.$el;
+        if(isArray(el)) {
+            for(var i = 0, l = el.length; i < l; i++) {
+                utils.addClass(el[i], className);
+            }
+            return;
+        }
         return utils.addClass(el, className);
     },
+
+    /**
+     * Remove a class to the current VM or to its child matching 'selector'
+     * @param {string} className
+     * @param {string} selector
+     */
     removeClass: function(className, selector) {
-        var el = selector ? this.$el.$findOne(selector) : this.$el;
+        var el = selector ? this.$find(selector) : this.$el;
+        if(isArray(el)) {
+            for(var i = 0, l = el.length; i < l; i++) {
+                utils.removeClass(el[i], className);
+            }
+            return;
+        }
         return utils.removeClass(el, className);
     }
   });
